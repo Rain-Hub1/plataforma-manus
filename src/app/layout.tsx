b7686af5a54763f821030b20b76fbe0d8dd3ebb0
Rain-Hub1 <rainhubsla@gmail.com>
2025-11-10 02:40:56 -0300

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -12,8 +12,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="pt-br">
+      <body className="bg-gray-900">{children}</body>
     </html>
   );
 }
